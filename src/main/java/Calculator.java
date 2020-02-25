e9c2dc884976c4a159e0f08980aa4f96592bd71c
crespo21 <tshepolesley21@gmail.com>
2020-02-25 08:53:25 +0200

    intial Calculator commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,41 +1,23 @@
-import java.util.Scanner;
-
 public class Calculator {
-//Index 3 out of bounds for length 3
-    public static int Add(int... numbers) {
+    public static  int add(int... numbers){
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i] ;
+        for (int i=0; i < numbers.length;i++){
+            sum += numbers[i];
         }
         return sum;
     }
 
-    public static int multiply(int... numbers) {
+    public static  int multiply(int... numbers){
         int product = 1;
-        for (int i = 0; i < numbers.length; i++) {
-            product *= numbers[1];
+        for (int i=0; i < numbers.length;i++){
+            product = product * numbers[i];
         }
         return product;
     }
 
-
     public static void main(String[] args) {
-
-        Scanner input = new Scanner((System.in));
-        int numbers = input.nextInt();
-
-
-        System.out.println("Enter numbers to add: "+Add(numbers));
-        System.out.println("your added total is: "+Add(numbers));
-
-        System.out.println("");
-        Add();
-        System.out.println("Enter numbers to multiply: "+ numbers);
-
-        System.out.println("your multiplied total is: "+numbers);
-
-
-
-
+        int[] numbers = {3,3};
+        System.out.println(add(numbers));
+        System.out.println(multiply(numbers));
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,13 @@
 
 import org.junit.jupiter.api.Test;
-import sun.awt.X11.XInputMethod;
-import sun.jvm.hotspot.utilities.Assert;
-
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class CalculatorTest {
     @Test
     void Add() {
-//        Calculator calc = new Calculator();
-       assertEquals (Calculator.Add(1,2),3);
-
+        assertEquals(7,Calculator.add(1,2,4));
     }
     @Test
     void multiply() {
-       assertEquals (Calculator.multiply(2,2),4);
+        assertEquals(9,Calculator.multiply(3,3));
     }
 }
